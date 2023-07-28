42ea2a65e50054b06a4fd3b2018d213f13942a73
Saikiran Rudra <sai@agilis.ai>
2023-07-28 15:41:42 +0530

    add sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO "USER" (USERNAME,PASSWORD,EMAIL,CREATED_AT,UPDATED_AT)
+VALUES
+('saikiranrudra','secrethai','saikiranrudra@gmail.com', CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP()),
+('rohanreddy','secrethai','rohanreddy@gmail.com', CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP()),
+('aravind','secrethai','webknot@gmail.com', CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP()),
+('pramod','secrethai','paramod@gmail.com', CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP()),
+('neel','secrethai','webknot@gmail.com', CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP()),
+('nidhi','secrethai','nidhi@gmail.com', CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP());
